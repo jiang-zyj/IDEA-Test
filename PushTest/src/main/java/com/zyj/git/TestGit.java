9cf2558334fc632e08a2df7faa35249301b12abe
dr_glb <dr_glb@zyj.com>
2021-05-19 20:03:44 +0800

    My first commit

---
+++ PushTest/src/main/java/com/zyj/git/TestGit.java
@@ -0,0 +1,21 @@
+package com.zyj.git;
+
+/**
+ * @ClassName TestGit
+ * @Auther: YaJun
+ * @Date: 2021 - 05 - 19 - 19:58
+ * @Description: com.zyj.git
+ * @version: 1.0
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        String sth = talkSth();
+        System.out.println(sth);
+    }
+
+    public static String talkSth() {
+        return "I have a good job";
+    }
+
+}
